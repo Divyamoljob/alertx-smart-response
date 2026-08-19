4b7066ce5913cbc8dd1664badce5b86641a14ecf
Divyamoljob <85997925+Divyamoljob@users.noreply.github.com>
2026-08-19 11:55:42 +0530

    Add/Update run_server.bat

---
+++ run_server.bat
@@ -0,0 +1,13 @@
+@echo off
+title AlertX Smart Response - Emergency & Crime System
+cls
+echo =====================================================================
+echo          ALERTX - SMART EMERGENCY & CRIME REPORTING SYSTEM
+echo =====================================================================
+echo.
+echo Initializing AlertX Python REST API Server & SQLite Database...
+echo.
+
+python server.py
+
+pause
